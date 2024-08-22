3729faceb984f6a18496b7ee6e5ccb3def1799bf
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2024-08-22 06:35:42 +0300

    Implement stact data structur

---
+++ stack/stack.dart
@@ -0,0 +1,27 @@
+class Stack<T> {
+  final List<T> _stack = [];
+  void push(T element) {
+    _stack.add(element);
+  }
+  T  pop() {
+    if (_stack.isEmpty) {
+   throw StateError("No elements in the Stack");
+    } else {
+      T lastElement = _stack.last;
+      _stack.removeLast();
+      return lastElement;
+    }
+  }
+
+  T top() {
+    if (_stack.isEmpty) {
+      throw StateError("No elements in the Stack");
+    } else {
+      return _stack.last;
+    }
+  }
+
+  bool isEmpty() {
+    return _stack.isEmpty;
+  }
+}
